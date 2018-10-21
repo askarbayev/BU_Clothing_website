6adbdb34ceb2902869f1882c76f21a869cd46c13
aburkato <aburkatovskiy@gmail.com>
2018-10-21 17:25:23 -0400

    added basic profile page functionality and logout

---
+++ scripts/profile.js
@@ -0,0 +1,21 @@
+window.onload = displayUserInfo; 
+
+function displayUserInfo() { 
+	var newLine = "<br/>";
+	var displayString = "Welcome!"
+	if(localStorage.getItem("email")){
+		var email = localStorage.getItem("email");
+		var userData = getUserInfo(email);
+		var name = userData[2];
+		var emailString = "Email: " + email;
+		var nameString = "Name: " + name;
+		displayString = displayString + newLine 
+		+ nameString + newLine
+		+ emailString + newLine;
+	}
+	document.getElementById("userInfo").innerHTML = displayString;
+}
+
+function logOut(){
+	localStorage.removeItem("email");
+}
