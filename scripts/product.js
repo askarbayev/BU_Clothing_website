fac35e867ea00c88378a3f1051ad69b5cc00db3e
aburkato <aburkatovskiy@gmail.com>
2018-11-03 22:20:34 +0100

    Added details and images to product pages

--- scripts/product.js
+++ scripts/product.js
@@ -1,13 +1,46 @@
 window.onload = displayProductInfo; 
 
 function displayProductInfo() { 
-	var h2 = document.getElementById("productName");
 	var url_string = window.location.href;
 	var url = new URL(url_string);
 	var productName = url.searchParams.get("productName");
 	if(productName == null){
-		h2.appendChild(document.createTextNode("No product selected!"));
+		var productNameElement = document.getElementById("productName");
+		productNameElement.appendChild(document.createTextNode("No product selected!"));
 		return;
 	}
-	h2.appendChild(document.createTextNode(productName));
+
+
+	var product = getProductByName(productName);
+	setProductName(product[NAME_INDEX]);
+	setProductCost(product[COST_INDEX]);
+	setProductdDescShort(product[DESC_SHORT_INDEX]);
+	setProductdDescLong(product[DESC_LONG_INDEX]);
+	setProductdImage(product[IMAGE_INDEX]);
+}
+
+function setProductName(productName){
+	var productNameElement = document.getElementById("productName");
+	productNameElement.appendChild(document.createTextNode(productName));
+}
+
+function setProductCost(cost){
+	var productCostElement = document.getElementById("cost");
+	productCostElement.appendChild(document.createTextNode(cost));
+}
+
+function setProductdDescShort(description){
+	var productDescriptionElement = document.getElementById("descShort");
+	productDescriptionElement.appendChild(document.createTextNode(description));
+}
+
+function setProductdDescLong(description){
+	var productDescriptionElement = document.getElementById("descLong");
+	productDescriptionElement.appendChild(document.createTextNode(description));
+}
+
+function setProductdImage(image){
+	var productImageElement = document.getElementById("image");
+	var imagePath = "./images/" + image;
+	productImageElement.src = imagePath;
 }
--- scripts/productDB.js
+++ scripts/productDB.js
@@ -1,10 +1,35 @@
 var products = 
 [
-["Product1", 5], 
-["Product2", 7], 
-["Product3", 100]
+["Hoodie", 34.99, "Clothing", "Boston University Colosseum Arch & Logo Pullover Hoodie", "It's game time! Make sure you're ready to rep your Boston University spirit by grabbing this impressive Colosseum Arch & Logo hoodie. The Boston University graphics alert anyone you encounter to your dedication to the greatest school around.", "Hoodie1.jpg"],
+["Long Sleeve T-shirt", 24.99, "Clothing", "Boston University Fanatics Branded Distressed Arch Over Logo Long Sleeve Hit T-Shirt", "Nothing gets you more excited than when it's time to go watch your beloved Boston University take on their opponents. Now you can boast your intense passion for the team everywhere you go with this unique Distressed Arch Over Logo long sleeve hit T-shirt from Fanatics Branded. The classic Boston University graphics printed on this tee will make your fandom apparent all season long!", "LongSleeveTsh1.jpg"],
+["Pants", 34.99, "Clothing", "Boston University Fanatics Branded Sideblocker Fleece Pants", "Show your love for the Boston University with these Sideblocker fleece pants from Fanatics Branded! The festive Boston University graphics will make your passionate fandom apparent. You'll be ready to rep your favorite team with these soft Boston University fleece pants!", "Pants.jpg"],
+["Jacket", 59.99, "Clothing", "Boston University Under Armour Threadborne Quarter-Zip Jacket", "Being a devoted Boston University fan is the best feeling in the world! Show others how it looks with this Threadborne quarter-zip jacket from Under Armour. This jacket features enough Boston University graphics so that everyone in your presence will be able to see from a mile away how much you love your favorite team!", "Jacket1.jpg"],
+["Hat", 19.99, "Clothing", "Boston University Top of the World Triple Threat Hat", "You love donning the best and brightest Boston University gear to every game. Now you can complete your look with this Triple Threat adjustable hat by Top of the World! This hat features a comfortable adjustable fit and raised embroidery that gives it that extra pop of fandom. Wear the mark of a true Boston University fan with the stellar look of this awesome hat.", "Hat1.jpg"],
+["Sport T-shirt", 24.99, "Clothing", "Boston University Custom Sport T-Shirt", "Sure you can go around and tell everyone how dedicated you are to the Boston University, but there is a much easier way to get your point across. Let everyone you pass know that you are a born and bred Boston University fan with this Custom Sport T-shirt! This tee boasts your team pride with Boston University graphics and the name and number of your choice. There are many ways to show your spirit, but this way you won't lose your voice.", "SportTsh1.jpg"],
+["Knit Hat", 22.99, "Clothing", "Boston University Top of the World Jack Frost Cuffed Knit Hat", "The pride you feel for the Boston University can't be compared by anyone in the stands. Now you can express that unwavering devotion, even when the temperature drops, when you put on this awesome Boston University Jack Frost cuffed knit hat from Top of the World. The embroidered Boston University graphics on this hat are bold and will show where your allegiances lie.", "Knithat1.jpg"],
+["Shorts", 44.99, "Clothing", "Boston University Under Armour MK-1 Shorts", "From sunup until sundown, you do your best to illustrate your unwavering Boston University pride everywhere you go. Continue to let everyone know that your fervent fandom will never fade when you're out being active by choosing these dynamic MK-1 shorts from Under Armour. Everyone will know which squad has your heart when they check out the impressive Boston University graphics decorating them!", "Shorts.jpg"],
+["Watch", 89, "Clothing", "Boston University 48MM Unisex Watch", "You have always been the #1 Boston University fan out there. Put that on display by sporting this 48MM Unisex Watch. Equipped with a sweet Boston University logo and colors this watch will instantly become one of your favorite team accessories in the building.", "Watch.jpg"],
+["Women T-shirt", 29.99, "Clothing", "Boston University Fanatics Branded Women's Plus Sizes Primary Team Logo T-Shirt", "Sure you can go around and tell everyone how dedicated you are to the Boston University, but there is a much easier way to get your point across. Let everyone you pass know that you are a born and bred Boston University fan with this Fanatics Branded Primary Team Logo T-shirt! This tee boasts your team pride with Boston University graphics. There are many ways to show your spirit, but this way you won't lose your voice.", "WTsh1.jpg"]
 ];
+
+var NAME_INDEX = 0;
+var COST_INDEX = 1;
+var CATEGORY_INDEX = 2;
+var DESC_SHORT_INDEX = 3;
+var DESC_LONG_INDEX = 4
+var IMAGE_INDEX = 5;
+
 
 function getAllProducts(){
 	return products;
+}
+
+function getProductByName(name){
+	var p = getAllProducts();
+	for(var i=0; i < p.length; i++){
+		if(p[i][NAME_INDEX] == name){
+			return p[i];
+		}
+	}
+	return null;
 }
