f880ec3153ed8704b085a9d0929c02c9d0de61d2
aburkato <aburkatovskiy@gmail.com>
2018-11-12 00:17:34 +0000

    Display the actual payment amount.

--- scripts/payment.js
+++ scripts/payment.js
@@ -2,13 +2,25 @@ window.onload = setTotal;
 
 function setTotal() { 
 	var total = 100;
-	if(localStorage.getItem("totalCost")){
-		total = localStorage.getItem("totalCost");
+
+	var products = getAllProducts();
+	var cartItems = document.getElementById("cart-items");
+	if (localStorage.getItem("cart-items")){
+		total = 0;
+		let items = JSON.parse(localStorage["cart-items"]);
+		console.log("Item: ", items);
+		for (let item of items){
+			let product = getProductByName(item);
+			let itemCost = product[1];
+			total = total + itemCost;
+		}
 	}
+
+	total = Math.round(total * 100) / 100;
     document.getElementById("totalCost").innerHTML = total;
 }
 
 function validatePayment() { 
-	localStorage.removeItem("totalCost");
+	localStorage.removeItem("cart-items");
     return true;
 }
